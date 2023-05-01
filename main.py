9f22b4f9199f57709a4f4241b812123742c2f1d9
chanhomin <alscksgh@gmail.com>
2023-05-01 11:22:17 +0900

    feat: Define print_hello() (Trailing comma test)
    print hello to all names in names.

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 #test python env
 
+def print_hello():
+	animals = ['dog', 'cat', 'hamster']
+	foods = ['banana',
+	 'apple',
+	 'spagetti'
+	]
+	names = [
+		'John',
+		'Jane',
+		'Gill-dong',
+	] #w/trailing comma
+	for  f_name in names:
+		print(f'hello, {f_name}')
 
-print('hello')
+if __name__ == '__main__':
+	print_hello()
 
 
 
